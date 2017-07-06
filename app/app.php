16a7ace9b87f8bec4ad143c7924fda76be884479
Larry Taylor and Nicole Freed <nicole@>
2017-07-06 09:22:52 -0700

    set app.php with root route

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Scrabble.php';
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use($app) {
+
+        return $app['twig']->render('input_form.html.twig');
+    });
+
+    return $app;
+?>
